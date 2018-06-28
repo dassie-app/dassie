b2fe978727488b882a21746e4416d64d7388bca5
Kyle Meenehan <kylemeenehan@gmail.com>
2018-06-28 19:53:53 +0200

    added profile photo field

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+def profile_photo_upload_handler(self, filename):
+    return 'static/images/%s/profile.jpg' % (self.user.username)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profileImage = models.ImageField(upload_to=profile_photo_upload_handler, blank=True)
