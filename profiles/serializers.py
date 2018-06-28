e2a45e43c001f36d93c82a874fa571b84b25f681
Kyle Meenehan <kylemeenehan@gmail.com>
2018-06-28 19:59:30 +0200

    fixed error in placement of profile_photo

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,10 +5,10 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email','first_name', 'last_name', 'profile_image')
+        fields = ('id', 'username', 'email','first_name', 'last_name')
 
 class ProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer()
     class Meta:
         model = Profile
-        fields = ('id', 'user')
+        fields = ('id', 'user', 'profile_image')
